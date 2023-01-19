4efe3678a86d59a1afcf6daabc2ba2a09f82bc1b
Tejas <tejasvispute25@gmail.com>
2023-01-20 00:35:52 +0530

    added

--- Snake-/Script.js
+++ Snake-/Script.js
@@ -1,41 +1,38 @@
-let inputDir = {x:0,y:0};
+let inputDir = { x: 0, y: 0 };
 let speed = 2;
 let lastpaintime = 0;
-let snakeArr = [{x:8,y:8}];//position of Snake
-let food = {x:3,y:5};
+let snakeArr = [{ x: 8, y: 9 }];//position of Snake
+let food = { x: 3, y: 5 };
 let score = 0;
+let scoreBox = document.getElementById("score")
 //starting logic
-function main(ctime)
-{
+function main(ctime) {
     window.requestAnimationFrame(main);
-    if((ctime-lastpaintime)/1000<1/speed)
-    {
+    if ((ctime - lastpaintime) / 1000 < 1 / speed) {
         return;
     }
 
-// console.log(ctime);
-lastpaintime = ctime;
-gameEngine();
+    // console.log(ctime);
+    lastpaintime = ctime;
+    gameEngine();
 
 }
+
+
 function isCollide(Snake) {
-    for(let i=0;i<snakeArr.length;i++)
-    {
-        if(Snake[i].x===Snake[i].x && Snake[i].y===Snake[i].y)
-        {
+    for (let i = 1; i < snakeArr.length; i++) {
+        if (Snake[i].x === Snake[0].x && Snake[i].y === Snake[0].y) {
             return true;
         }
-       
+
     }
-    if(Snake[0].x>=10 || Snake[0].y<=0||Snake[0].y>=10 || Snake[0].x<=0)
-    {
+    if (Snake[0].x >= 10 || Snake[0].y <= 0 || Snake[0].y >= 10 || Snake[0].x <= 0) {
         return true;
     }
     return false;
 
-    
+
 }
-console.log(score);
 // function gameEngine()
 // {
 //     // if(iscollide(SnakeArr))
@@ -46,7 +43,7 @@ console.log(score);
 //     //     score = 0;
 
 //     // }
-  
+
 
 //     // If you have eaten the food, increment the score and regenerate the food
 //     if(SnakeArr[0].y === food.y && SnakeArr[0].x ===food.x){
@@ -72,7 +69,7 @@ console.log(score);
 //     SnakeArr[0].x += direction.x;
 //     SnakeArr[0].y += direction.y;
 
-    
+
 //     //logic 1 Snake Face
 //     row.innerHTML ="";
 //     SnakeArr.forEach((e,index)=>{
@@ -87,7 +84,7 @@ console.log(score);
 //         {
 //             SnakeElement.classList.add('snake');
 //         }
-        
+
 //         row.appendChild(SnakeElement);
 
 //     });
@@ -111,32 +108,32 @@ console.log(score);
 // window.requestAnimationFrame(main);
 // window.addEventListener('keydown',e=>
 // {
-    
+
 // direction = {x:0,y:1};
 // switch(e.key) {
 //     case "ArrowUp":
 //         console.log("Keyup");
 //     direction.x=0;
 //     direction.y=-1;
-  
+
 //     break;
 //     case "ArrowDown":
 //         console.log("Keydown");
 //     direction.x=0;
 //     direction.y=1;
- 
+
 //     break;
 //     case "ArrowLeft":
 //         console.log("Keyleft");
 //     direction.x=-1;
 //     direction.y=0;
-  
+
 //     break;
 //     case "ArrowRight":
 //         console.log("KeyRight");
 //     direction.x=1;
 //     direction.y=0;
-   
+
 //     break;
 //     default:
 //         break;
@@ -146,37 +143,38 @@ console.log(score);
 // }}
 
 // );
-function gameEngine(){
+function gameEngine() {
     // Part 1: Updating the snake array & Food
-    if(isCollide(snakeArr)){
+    if (isCollide(snakeArr)) {
         // gameOverSound.play();
         // musicSound.pause();
-        // inputDir =  {x: 0, y: 0}; 
+        inputDir = { x: 0, y: 0 };
         alert("Game Over. Press any key to play again!");
-        // snakeArr = [{x: 8, y: 8}];
+        snakeArr = [{ x: 8, y: 8 }];
         // musicSound.play();
-        score = 0; 
+        score = 0;
     }
 
     // If you have eaten the food, increment the score and regenerate the food
-    if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
+    if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
         // foodSound.play();
         score += 1;
+        console.log(score)
         // if(score>hiscoreval){
         //     hiscoreval = score;
         //     localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
         //     hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
         // }
-        // scoreBox.innerHTML = "Score: " + score;
-        snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
+        scoreBox.innerHTML = "Score: " + score;
+        snakeArr.unshift({ x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y });
         let a = 2;
         let b = 8;
-        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
+        food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) }
     }
 
     // Moving the snake
-    for (let i = snakeArr.length - 2; i>=0; i--) { 
-        snakeArr[i+1] = {...snakeArr[i]};
+    for (let i = snakeArr.length - 2; i >= 0; i--) {
+        snakeArr[i + 1] = { ...snakeArr[i] };
     }
 
     snakeArr[0].x += inputDir.x;
@@ -185,15 +183,15 @@ function gameEngine(){
     // Part 2: Display the snake and Food
     // Display the snake
     board.innerHTML = "";
-    snakeArr.forEach((e, index)=>{
+    snakeArr.forEach((e, index) => {
         snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
 
-        if(index === 0){
+        if (index === 0) {
             snakeElement.classList.add('head');
         }
-        else{
+        else {
             snakeElement.classList.add('snake');
         }
         board.appendChild(snakeElement);
@@ -222,8 +220,8 @@ function gameEngine(){
 // }
 
 window.requestAnimationFrame(main);
-window.addEventListener('keydown', e =>{
-    inputDir = {x: 0, y: 1} // Start the game
+window.addEventListener('keydown', e => {
+    inputDir = { x: 0, y: 1 } // Start the game
     // moveSound.play();
     switch (e.key) {
         case "ArrowUp":
